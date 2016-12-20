785be71d13b1ce8811f16b4e9186e4ae9915f8e1
kr1stjans <kristjansesek@gmail.com>
2016-12-20 18:05:49 +0100

    File plotting v1

--- realtime_plotting.py
+++ realtime_plotting.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 -u
 import sys
 import matplotlib.pyplot as plt
 import collections
+import fileinput
+import time
 
 X_AXIS_SIZE = 100
-Y_AXIS_SIZE = 2
+Y_AXIS_SIZE = 10
 
 plt.ion()
 fig = plt.figure()
-
-if not sys.stdin.isatty():
-    input_stream = sys.stdin
-else:
-    raise IndexError('Can only be used with bash!')
 
 x_axis = list(range(X_AXIS_SIZE))
 
@@ -24,13 +21,17 @@ plt.axis([0, X_AXIS_SIZE, -Y_AXIS_SIZE, Y_AXIS_SIZE])
 fig.canvas.draw()
 plt.show(block=False)
 
-for line in input_stream:
-    splitted_line = line.split(' ')
-    try:
-        measurement = float(splitted_line[len(splitted_line) - 1])
-        y_axis.append(measurement)
-        li.set_ydata(y_axis)
-        fig.canvas.draw()
-        plt.pause(0.0001)
-    except ValueError:
-        pass
+
+with open('../realtime_plotting/sample/teammaker.txt', 'r') as f:
+    while 1:
+        line = f.readline()
+        if line:
+            splitted_line = line.split(' ')
+            try:
+                measurement = float(splitted_line[len(splitted_line) - 1])
+                y_axis.append(measurement)
+                li.set_ydata(y_axis)
+                fig.canvas.draw()
+                plt.pause(0.0001)
+            except ValueError:
+                pass
